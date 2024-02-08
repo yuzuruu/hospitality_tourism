8478c302e46b31a455939c103dff546b428c6097
Yuzuru Utsunomiya <yuzuru@nagasaki-u.ac.jp>
2024-02-08 11:13:32 +0900

    arranged in accordance with data composition.

--- hospitality_tourism.r
+++ hospitality_tourism.r
@@ -1,174 +1,124 @@
+################################
 # hospitality tourism
-# 12th. January 2023
-# Yuzuru Utsunomiya
-
-
+# Original: 12th. January 2023
+# Revise: 6th. February 2024
+# Yuzuru Utsunomiya, Ph. D.
+# (Faculty of Economics, Nagasaki University)
+################################
+#
 # ---- read.library ----
 library(tidyverse)
 library(khroma)
 library(sf)
-
+# 
 # ---- read.data ----
-trial_walk_wheelchair <- 
+walk_wheelchair <- 
   readxl::read_excel(
     "hospitality_tourism.xlsx",
-    sheet = "trial_nu"
-    ) %>% 
-  # The waypoints data by GPS contains ymd with the same hms and 
-  # hms with the same ymd. To merge them correctly, we once 
-  # obtain subset of the variables.
-  dplyr::mutate(
-    # obtain ymd only
-    date = lubridate::as_date(
-      stringr::str_sub(
-        date, 
-        start = 1, 
-        end = 10
-        )
-      ),
-    # hms only
-    utc = hms::as_hms(
-      stringr::str_sub(
-        utc, 
-        start = 12, 
-        end = 20
-        )
-      ),
-    mode = factor(mode)
-    ) %>% 
-  # merge the ymd and hms together
-  dplyr::mutate(
-    utc = lubridate::ymd_hms(paste(date, utc))
+    sheet = "Nagasaki"
   ) %>% 
   dplyr::mutate(
-    utc = dplyr::case_when(
-      # provide the lag second (2 this time) manually.
-      # In the future, we would like to do that automatically.
-      mode == "walk" ~ utc + lubridate::seconds(2), 
-      TRUE ~ utc
-      )
-  ) %>% 
-  dplyr::select(-date) 
-
-# ---- handle.data ----
-# walk
-# obtain 5-seconds-lagged variables to calculate distance
-trial_walk <- 
-  trial_walk_wheelchair %>% 
-  dplyr::filter(
-    mode == "walk"
-    ) %>% 
-  dplyr::mutate(
-    lat_5sec = dplyr::lag(latitude, 1),
-    lon_5sec = dplyr::lag(longitude, 1)
-  ) %>% 
-  na.omit()
-# select overlapped period
-# original
-df1 <- trial_walk %>%
-  # transform the pairs of lon/at into sf
-  sf::st_as_sf(
-    coords = c("longitude","latitude"), 
-    crs = 4326
-    ) 
-# 5 seconds after
-df2 <- trial_walk %>%
-  sf::st_as_sf(
-    coords = c("lon_5sec","lat_5sec"), 
-    crs = 4326
+    round = dplyr::case_when(
+      round == "1" ~ "first",
+      round == "2" ~ "second",
+      round == "3"  ~ "third",
+      TRUE  ~ "NA"
     )
-# calculate speed from moving distance per 5 seconds
-speed_walk <- 
-  # the sf::st_distance function returns distance wih unit (m by default).
-  # We do not need the unit and remove the unit using as.numeric() function.
-  as.numeric(
-    diag(
-      # compute velocity
-      # GPS records positions every 5 seconds.
-      # To translate the distance into velocity, we multiplied 
-      # 12 times (60 min / 5 min = 12)
-      # 60 minutes
-      # 1/1000
-      12*60*sf::st_distance(x = df1, y = df2)/1000
-      )
-    )
-# add the calculation results above to the original data
-trial_walk_speed <- 
-  trial_walk %>% 
-  dplyr::bind_cols(speed = speed_walk)
-#
-# wheelchair
-trial_wheelchair <- 
-  trial_walk_wheelchair %>% 
-  dplyr::filter(mode == "wheelchair") %>% 
-  dplyr::mutate(
-    lat_5sec = lag(latitude, 1),
-    lon_5sec = lag(longitude, 1)
-  ) %>% 
-  na.omit()
-df1 <- trial_wheelchair %>%
-  st_as_sf(coords = c("longitude","latitude"), crs = 4326) 
-df2 <- trial_wheelchair %>%
-  st_as_sf(coords = c("lon_5sec","lat_5sec"), crs = 4326)
-speed_wheelchair <- as.numeric(diag(12*60*st_distance(x = df1, y = df2)/1000))
-trial_wheelchair_speed <- 
-  trial_wheelchair %>% 
-  dplyr::bind_cols(
-    speed = speed_wheelchair
-    )
-# merge the files above
-df_trial_walk_wheelchair <- 
-  trial_wheelchair_speed %>% 
-  bind_rows(trial_walk_speed)
-# save the df_trial_walk_wheelchair as .csv for reuse
-write_excel_csv(
-  df_trial_walk_wheelchair, 
-  "df_trial_walk_wheelchair.csv"
   )
 
-# ---- check.overlap ----
-# calculate overlap period
-# GPS often records points in unnecessary periods and data contains the
-# unnecessary periods. We do not need it and need to remove the data.
-# To remove that, we calculate period where period of waypoints of 
-# wheelchair / walk overlap.
-overlap_period <- 
-  df_trial_walk_wheelchair %>% 
-  group_by(mode) %>% 
-  summarize(
-    Min. = min(utc),
-    Max. = max(utc)
+# obtain data for computation
+walk_wheelchair_standard_time <- 
+  walk_wheelchair %>% 
+  # split the data by assigned group
+  # This time, because of data composition, we set "mode" as a group.
+  # The "mode" refers to ways of going around; wheelchair and walk.
+  # As the survey will progress, we need to add other groups including......
+  # 1. course 
+  # 2. round (1st., 2nd., and 3rd.)
+  # 3. city
+  group_by(mode, course, round) %>%
+  # add variables
+  dplyr::mutate(
+    # Compute differences between start time and a reference time
+    difference = lubridate::time_length(
+      lubridate::interval(
+        # set a reference time deep in the past
+        lubridate::ymd_hms("2022-01-01 00:00:00"), 
+        # minimum utc = start time
+        # utc refers to times GPS logged with locations and other information.
+        min(utc)
+      )
+    ),
+    # Compute the gap between utc and difference.
+    # We use the standard time to compare velocity with variety of backgrounds with each other. 
+    standard_time = (utc - seconds(difference))
+  ) %>% 
+  dplyr::select(-difference) %>% 
+  ungroup() %>% 
+  tidyr::complete(
+    # Fill data into missing values.
+    # To avoid malfunction, even for 5-seconds-frequency data, 
+    # we fill variables every 1 second.
+    standard_time = tidyr::full_seq(standard_time, period = 1),
+    mode,
+    course,
+    round
+    ) %>% 
+  # convert the standard time in dttm format into one in hms format.
+  dplyr::mutate(standard_time = hms::as_hms(standard_time))
+# save the results in rds format
+# This section is useful for huge data which is unavailable to save in csv format.
+readr::write_rds(
+  walk_wheelchair_standard_time,
+  "walk_wheelchair_standard_time.rds"
+)
+# save the results in csv format
+readr::write_excel_csv(
+  walk_wheelchair_standard_time, 
+  "walk_wheelchair_standard_time.csv"
   )
-df_trial_walk_wheelchair <- 
-  df_trial_walk_wheelchair %>%
-  # pick overlapped period
-  dplyr::filter(
-    utc > max(overlap_period$Min.) & utc < min(overlap_period$Max.)
-    )
-# ---- draw.line ---- 
+
+# ----- line.plot -----
 line_trial_walk_wheelchair <- 
-  df_trial_walk_wheelchair %>% 
-  ggplot(
+  walk_wheelchair_standard_time %>% 
+  ggplot2::ggplot(
     aes(
-      x = utc,
-      y = speed,
-      color = mode
+      x = standard_time,
+      y = log(velocity+0.5)
     )
-  ) + 
-  geom_line() +
-  geom_point() +
+  ) +
+  # add lines
+  geom_line(aes(color = round)) +
+  # add points
+  geom_point(aes(color = round)) +
+  # apply a color paletter by Prof. Okabe and Prof. Ito
   scale_color_okabeito() +
-  labs(x = "Time (Unit: 5 seconds)", y = "Velocity (Unit: km/h)") +
+  # add names of xy axes
+  labs(
+    x = "Time (Unit: 1 seconds)", 
+    y = "Velocity (log Trans., Unit: km/h)",
+    caption = "by Yuzuru Utsunomiya, Ph. D."
+  ) +
+  # conventional theme named "theme=classic"
   theme_classic() + 
+  # place legends under the x axis
   theme(
-    legend.position = "bottom"
-  )
-# save the figure
+    legend.position = "bottom",
+    strip.background = element_blank()
+  ) +
+  facet_wrap(~ mode + course, scales = "free_x")
+line_trial_walk_wheelchair
+# 
+# save
 ggsave(
-  "line_df_trial_walk_wheelchair.pdf",
+  # file name
+  "line_trial_walk_wheelchair.pdf",
+  # target object
   plot = line_trial_walk_wheelchair,
-  height = 200,
-  width = 200,
+  height = 500,
+  width = 500,
   units = "mm"
 )
+# END
+
 
